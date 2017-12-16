b91e3bb97f9eea631888af3a5ff2c5ca298e148c
Ripbyrustavi <murathalitcan@gmail.com>
2017-12-16 02:25:37 +0200

    Update komutuyenidenbaşlat.js

--- ozellikler/komutuyenidenbaşlat.js
+++ ozellikler/komutuyenidenbaşlat.js
@@ -1,6 +1,6 @@
 const eks = require("./ekstra.json");
 exports.run = (client, message, args) => {
-  if(message.author.id !== 'eks.olusturucu') return;
+  if(message.author.id !== eks.olusturucu) return;
   if(!args || args.size < 1) return message.reply('Komut ismi gir.');
   delete require.cache[require.resolve(`./${args[0]}.js`)];
   message.reply(`**Yeniden başlatıldı**`);
